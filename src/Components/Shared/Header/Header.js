d1001266c173a1a19e28f73ff6db38b94ab6d4bd
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-04-17 21:25:44 +0600

    navbar making

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import "./Header.css"
+import logo from "../../../images/logo.png";
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
+        <div className='header'>
+            <Navbar collapseOnSelect expand="lg" className='p-3'  sticky='top' variant="dark">
+                <Container>
+                    <Navbar.Brand className='d-flex align-items-center '>
+                        <img height={50} src={logo} alt="" />
+                        Shutter Studio
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">Blog</Nav.Link>
+                            <Nav.Link href="#deets">About </Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">Log In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 };
