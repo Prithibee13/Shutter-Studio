0e997c353bdb60637a7d2e9dd0bd5309afbb7ec2
Prithibee13 <66945275+Prithibee13@users.noreply.github.com>
2022-04-17 18:22:47 +0600

    create fundamental folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+    <Header></Header>
       <Routes>
         <Route></Route>
         <Route></Route>
